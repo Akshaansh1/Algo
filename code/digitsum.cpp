25bb4172a0dc20797f1e552e04f2297c5ce314bc
Akshaansh1 <srivastava.akshaansh@gmail.com>
2023-11-03 17:58:17 +0530

    Added

---
+++ code/digitsum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+int main()
+{
+    ll t;
+    cin>>t;
+
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+
+        
+        cout<<(n+1)/10<<endl;
+    }
+
+}
